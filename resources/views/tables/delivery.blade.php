0ed0f365a9a5f2674adaee5af28239957e82e810
YuliusCaesar01 <ycaesar01@gmail.com>
2024-07-03 12:26:27 +0700

    3/7/2024

--- resources/views/tables/delivery.blade.php
+++ resources/views/tables/delivery.blade.php
@@ -27,7 +27,7 @@
                     <div class="col-12">
                         <div class="card">
                             <div class="card-header">
-                                <h3 class="card-title">Monitor</h3>
+                                <h3 class="card-title">Delivery</h3>
                             </div>
                             <!-- /.card-header -->
                             {{-- <div class="card-body" style="overflow-x:auto; height:385px;"> --}}
